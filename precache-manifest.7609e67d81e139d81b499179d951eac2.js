2d50bd1b02f865015274e4d83fa5977ee6e929cb
Leonardo <leonardo.lopes@uppercase.com.br>
2019-12-11 10:22:47 -0300

    Updates

---
+++ precache-manifest.7609e67d81e139d81b499179d951eac2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1006a0fbc2cbe4076538d5ddd21e9abf",
+    "url": "/ReactJS-Starter/index.html"
+  },
+  {
+    "revision": "218a84f25c8723191ba9",
+    "url": "/ReactJS-Starter/static/js/2.f3b9d789.chunk.js"
+  },
+  {
+    "revision": "b0507c3bca7389e41c39",
+    "url": "/ReactJS-Starter/static/js/main.4a328784.chunk.js"
+  },
+  {
+    "revision": "d8d2c99d9dd341068f5a",
+    "url": "/ReactJS-Starter/static/js/runtime-main.61360150.js"
+  }
+]);
